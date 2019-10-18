40e56aa3707f92e3440d55c3ad1b5f5aeb7d8d18
Diane Lee <dlee24@nd.edu>
2019-10-17 20:23:29 -0400

    Uncommented script

--- proteomestest.sh
+++ proteomestest.sh
@@ -11,9 +11,11 @@ cat mcrA*.fasta > combined_mcrA.fasta
 for combined_gene in combined*.fasta
 do
 SeqName=$(echo $combined_gene | sed -E 's/\.fasta//g')
-echo $SeqName
-muscle -in $combined_gene -out $SeqName.afa | hmmbuild built$SeqName.hmm $SeqName.afa
+muscle -in $combined_gene -out $SeqName.afa
 done
+
+hmmbuild builtcombined_hsp.hmm combined_hsp.afa
+hmmbuild builtcombined_mcrA.hmm combined_mcrA.afa
 
 
 mv built*.hmm ../proteomes
@@ -26,9 +28,47 @@ hmmsearch --tblout table_mcrA_$ProteomeName.txt builtcombined_mcrA.hmm $proteome
 hmmsearch --tblout table_hsp_$ProteomeName.txt builtcombined_hsp.hmm $proteome
 done
 
-for tables in table_*.txt
-cat $tables | grep -v "\#" | wc -l >> hsp_match_table.txt
+for tables in table_mcrA_proteome_*.txt
+do
+ProtName=$(echo $tables | sed -E 's/table_mcrA_.+_//g'| sed -E 's/\.txt//g')
+mcrA_matches=$(cat $tables | grep -v "\#" | wc -l)
+echo $ProtName","$mcrA_matches >> final_mcrA.csv
+done
 
-for tables in table_mcrA*.txt
+for tables in table_hsp_proteome_*.txt
+do
+ProtName=$(echo $tables | sed -E 's/table_hsp_.+_//g'| sed -E 's/\.txt//g')
+hsp_matches=$(cat $tables | grep -v "\#" | wc -l)
+echo $ProtName","$hsp_matches >> final_hsp.csv
+done
+
+touch listofproteomes.csv
+join -t , -1 1 -2 1 final_mcrA.csv final_hsp.csv | head -n 50 | sort -nr -t , -k2,2 -k3 >> listofproteomesnoheader.csv
+mv listofproteomesnoheader.csv ..
+cd ..
+
+echo "Proteome,mcrA hits,hsp70 hits" > ProteomeListTitled.csv
+cat listofproteomesnoheader.csv >> ProteomeListTitled.csv
+echo "Ordered list of Proteomes by presence of mcrA then hsp70 genes"
+cat ProteomeListTitled.csv
+
+cat listofproteomesnoheader.csv | grep -E ".+, [^0], [^0]" > recommended_table_no_header.csv
+echo "pH-Resistant Methanogenic Proteomes" > recommended_candidates.txt
+cat recommended_table_no_header.csv | cut -d , -f 1 >> recommended_candidates.txt
 
 
+echo "
+
+See recommended_candidates.txt for a list of pH-resistant methanogenic proteomes,
+and summary table ProteomeListTitled.csv for full results."
+
+rm recommended_table_no_header.csv
+rm listofproteomesnoheader.csv
+
+cd ./proteomes
+rm *.csv
+rm *.txt
+mv *.hmm ..
+
+cd ../ref_sequences
+rm combined*
--- ref_sequences/aligner.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-#usage: bash aligner.sh *.fasta
-for refseqs in *.fasta
-do
-./muscle3.8.31_i86linux64 -in $refseqs >> aligned.fasta
-done
